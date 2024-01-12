aaa82b7739133a6e50bb533c160c42d22f64a80b
STIKA <graysky9@naver.com>
2024-01-12 19:26:02 +0900

    1.12_수정

---
+++ teamproject/Unit.cs
@@ -0,0 +1,51 @@
+﻿namespace DietDungeon
+{
+    public abstract class Unit
+    {
+        protected string name;
+        protected int level = 1;
+        protected int atk;
+        protected int hp;
+
+        public int Level { get => level; set => level = value; }
+        public String Name { get => name; set => name = value; }
+        public int Atk { get => atk; set => atk = value; }
+        public int Hp { get => hp; set => hp = value; }
+
+        public abstract void Print();
+        public void Attack(Unit target)
+        {
+            var rand = new Random();
+            var error = (int)(Math.Ceiling(atk / 10.0));
+            var damage = rand.Next(atk - error, atk + error);
+            var targetHealth = Math.Max(target.hp - damage, 0);
+
+            Console.WriteLine();
+            Console.WriteLine($"{name}의 공격!");
+            Console.WriteLine($"{target.Name} 을(를) 맞췄습니다. [데미지 : {damage}]");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{target.Level} {target.Name}");
+            Console.Write($"Hp {target.hp} -> ");
+
+
+
+            if (targetHealth > 0)
+            {
+                Console.WriteLine($"{targetHealth}");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine($"Dead");
+                Console.WriteLine();
+            }
+
+            target.Hp = targetHealth;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone(); //랜덤 몬스터 동시 적용돼서 클론으로 적용
+        }
+    }
+}
