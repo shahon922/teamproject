6bfa3d1562f9fa6ae29e353ee027e8f7496266d0
ParkChang-Hyeon <154569535+ParkChang-Hyeon@users.noreply.github.com>
2024-01-16 02:26:51 +0900

    1.16 Json 게임 저장하기
    추가로 조금 더 손을 봐야 할 거 같습니다.

---
+++ teamproject/FileUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace DietDungeon
+{
+    public static class FileUtil
+    {
+        private static readonly string playerDataPath = "playerData.json";
+        // player 객체를 파일에 저장
+        public static void SavePlayer(Player player)
+        {
+            string strJson = JsonSerializer.Serialize<Player>(player, new JsonSerializerOptions() { WriteIndented = true });
+            File.WriteAllText(playerDataPath, strJson);
+        }
+
+        // 파일에서 player 객체를 불러옴
+        public static Player? LoadPlayer()
+        {
+            if (File.Exists(playerDataPath))
+            {
+                string json = File.ReadAllText(playerDataPath);
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    Player? player = JsonSerializer.Deserialize<Player>(json);
+                    return player;
+                }
+            }
+
+            return null;
+        }
+
+        public static Boolean ExistPlayer()
+        {
+            return File.Exists(playerDataPath);
+        }
+    }
+}
